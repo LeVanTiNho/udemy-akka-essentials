62d4b1d89643cd867356fdb6614d7a1105161f22
tinhot97@gmail.com <tinhot97@gmail.com>
2020-01-15 22:32:26 +0700

    Making progress in learning Akka framework

--- src/main/scala/part2actors/ActorLoggingDemo.scala
+++ src/main/scala/part2actors/ActorLoggingDemo.scala
@@ -3,19 +3,25 @@ package part2actors
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import akka.event.Logging
 
+// Lesson 6
 object ActorLoggingDemo extends App {
 
   class SimpleActorWithExplicitLogger extends Actor {
     // #1 - explicit logging
+
+    // Logging object is the main entry point for akka logging
+    // The apply method of this object will return a LoggingAdapter
     val logger = Logging(context.system, this)
 
     override def receive: Receive = {
       /*
+      Log levels:
         1 - DEBUG
         2 - INFO
         3 - WARNING/WARN
         4 - ERROR
        */
+      // case message => logger.warning(message.toString)// LOG it
       case message => logger.info(message.toString)// LOG it
     }
   }
@@ -28,7 +34,8 @@ object ActorLoggingDemo extends App {
   // #2 - ActorLogging
   class ActorWithLogging extends Actor with ActorLogging {
     override def receive: Receive = {
-      case (a, b) => log.info("Two things: {} and {}", a, b) // Two things: 2 and 3
+      // log method of ActorLogging return a LoggingAdapter
+      case (a, b) => log.info("Two things: {} and {}", a, b) // string interpolation
       case message => log.info(message.toString)
     }
   }
@@ -38,4 +45,9 @@ object ActorLoggingDemo extends App {
 
   simplerActor ! (42, 65)
 
+  /**
+    * Notes:
+    *   - The logging is asynchronously -> we can except some order of the log message
+    *   - The Logging module doesn't depend on the actor implementation -> so we can use another logging module
+    */
 }
--- src/main/scala/part2actors/IntroAkkaConfig.scala
+++ src/main/scala/part2actors/IntroAkkaConfig.scala
@@ -11,55 +11,61 @@ object IntroAkkaConfig extends App {
     }
   }
 
-  /**
-    * 1 - inline configuration
-    */
-  val configString =
-    """
-      | akka {
-      |   loglevel = "ERROR"
-      | }
-    """.stripMargin
-
-  val config = ConfigFactory.parseString(configString)
-  val system = ActorSystem("ConfigurationDemo", ConfigFactory.load(config))
-  val actor = system.actorOf(Props[SimpleLoggingActor])
-
-  actor ! "A message to remember"
-
-  /**
-    * 2 - config file
-    */
-
-  val defaultConfigFileSystem = ActorSystem("DefaultConfigFileDemo")
-  val defaultConfigActor = defaultConfigFileSystem.actorOf(Props[SimpleLoggingActor])
-  defaultConfigActor ! "Remember me"
+//  /**
+//    * 1 - inline configuration
+//    */
+//  val configString =
+//    """
+//      | akka {
+//      |   loglevel = "ERROR"
+//      | }
+//    """.stripMargin
+//
+//  // ConfigFactory.parseString method returns a Config object (type-safe config)
+//  val config = ConfigFactory.parseString(configString)
+//
+//  // ActorSystem apply method can receive a Config object
+//  val system = ActorSystem("ConfigurationDemo", ConfigFactory.load(config))
+//
+//  val actor = system.actorOf(Props[SimpleLoggingActor])
+//
+//  actor ! "A message to remember"
+//
+//  /**
+//    * 2 - config file
+//    */
+//    // Default config file is resources/application.conf
+//  val defaultConfigFileSystem = ActorSystem("DefaultConfigFileDemo")
+//  val defaultConfigActor = defaultConfigFileSystem.actorOf(Props[SimpleLoggingActor])
+//  defaultConfigActor ! "Remember me"
 
   /**
     * 3 - separate config in the same file
-    */
-  val specialConfig = ConfigFactory.load().getConfig("mySpecialConfig")
-  val specialConfigSystem = ActorSystem("SpecialConfigDemo", specialConfig)
-  val specialConfigActor = specialConfigSystem.actorOf(Props[SimpleLoggingActor])
-  specialConfigActor ! "Remember me, I am special"
+  */
+    // ConfigFactory.load() loads configs from the default file config application.conf
+  val config3 = ConfigFactory.load().getConfig("mySpecialConfig")
+  val system3 = ActorSystem("system3", config3)
+  val actor3 = system3.actorOf(Props[SimpleLoggingActor])
+  actor3 ! "separate config in the same file demo"
 
-  /**
-    * 4 - separate config in another file
-    */
 
-  val separateConfig = ConfigFactory.load("secretFolder/secretConfiguration.conf")
-  println(s"separate config log level: ${separateConfig.getString("akka.loglevel")}")
-
-  /**
-    * 5 - different file formats
-    * JSON, Properties
-    */
-  val jsonConfig = ConfigFactory.load("json/jsonConfig.json")
-  println(s"json config: ${jsonConfig.getString("aJsonProperty")}")
-  println(s"json config: ${jsonConfig.getString("akka.loglevel")}")
-
-  val propsConfig = ConfigFactory.load("props/propsConfiguration.properties")
-  println(s"properties config: ${propsConfig.getString("my.simpleProperty")}")
-  println(s"properties config: ${propsConfig.getString("akka.loglevel")}")
+//  /**
+//    * 4 - separate config in another file
+//    */
+//
+//  val separateConfig = ConfigFactory.load("secretFolder/secretConfiguration.conf")
+//  println(s"separate config log level: ${separateConfig.getString("akka.loglevel")}")
+//
+//  /**
+//    * 5 - different file formats
+//    * JSON, Properties
+//    */
+//  val jsonConfig = ConfigFactory.load("json/jsonConfig.json")
+//  println(s"json config: ${jsonConfig.getString("aJsonProperty")}")
+//  println(s"json config: ${jsonConfig.getString("akka.loglevel")}")
+//
+//  val propsConfig = ConfigFactory.load("props/propsConfiguration.properties")
+//  println(s"properties config: ${propsConfig.getString("my.simpleProperty")}")
+//  println(s"properties config: ${propsConfig.getString("akka.loglevel")}")
 
 }
