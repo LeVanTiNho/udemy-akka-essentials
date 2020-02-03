9f51def2cd986cf44b1ae7c6d90425c977a2331d
tinhot97@gmail.com <tinhot97@gmail.com>
2020-02-03 23:03:28 +0700

    Making progress in learning Akka framework

--- src/main/scala/part5infra/Dispatchers.scala
+++ src/main/scala/part5infra/Dispatchers.scala
@@ -1,6 +1,7 @@
 package part5infra
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import com.typesafe.config.ConfigFactory
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
@@ -20,11 +21,13 @@ object Dispatchers extends App {
     }
   }
 
-  val system = ActorSystem("DispatchersDemo") // , ConfigFactory.load().getConfig("dispatchersDemo")
+  val system = ActorSystem("DispatchersDemo", ConfigFactory.load().getConfig("dispatchersDemo"))
 
-  // method #1 - programmatically/in code
+  // method #1 - programmatically / in code
 
   val actors = for (i <- 1 to 10) yield system.actorOf(Props[Counter].withDispatcher("my-dispatcher"), s"counter_$i")
+  // the ten actors share my-dispatcher
+
   val r = new Random()
   for (i <- 1 to 1000) {
     actors(r.nextInt(10)) ! i
@@ -33,32 +36,33 @@ object Dispatchers extends App {
   // method #2 - from config
   val rtjvmActor = system.actorOf(Props[Counter], "rtjvm")
 
+
   /**
     * Dispatchers implement the ExecutionContext trait
     */
 
-  class DBActor extends Actor with ActorLogging {
-
-    // solution #1
-    implicit val executionContext: ExecutionContext = context.system.dispatchers.lookup("my-dispatcher")
-    // solution #2 - use Router
-
-    override def receive: Receive = {
-      case message => Future {
-        // wait on a resource
-        Thread.sleep(5000)
-        log.info(s"Success: $message")
-      }
-    }
-  }
-
-  val dbActor = system.actorOf(Props[DBActor])
-//  dbActor ! "the meaning of life is 42"
-
-  val nonblockingActor = system.actorOf(Props[Counter])
-  for (i <- 1 to 1000) {
-    val message = s"important message $i"
-    dbActor ! message
-    nonblockingActor ! message
-  }
+//  class DBActor extends Actor with ActorLogging {
+//
+//    // solution #1
+//    implicit val executionContext: ExecutionContext = context.system.dispatchers.lookup("my-dispatcher")
+//    // solution #2 - use Router
+//
+//    override def receive: Receive = {
+//      case message => Future {
+//        // wait on a resource
+//        Thread.sleep(5000)
+//        log.info(s"Success: $message")
+//      }
+//    }
+//  }
+//
+//  val dbActor = system.actorOf(Props[DBActor])
+////  dbActor ! "the meaning of life is 42"
+//
+//  val nonblockingActor = system.actorOf(Props[Counter])
+//  for (i <- 1 to 1000) {
+//    val message = s"important message $i"
+//    dbActor ! message
+//    nonblockingActor ! message
+//  }
 }
